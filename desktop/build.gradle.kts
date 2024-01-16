6916e5b516cf4609b44c0d08ca4b995fe5157111
Neo <c.min.tseng@gmail.com>
2024-01-16 21:58:59 +0800

    move Main to package &　modify mainClass path PS. can not run at windows system !! lost "skiko-windows-x64.dll.sha256"

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -30,7 +30,7 @@ kotlin {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "funny.buildapp.worklite.MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "common"
--- desktop/src/jvmMain/kotlin/funny/buildapp/worklite/Main.kt
+++ desktop/src/jvmMain/kotlin/funny/buildapp/worklite/Main.kt
@@ -1,4 +1,4 @@
-
+package funny.buildapp.worklite
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -11,6 +11,7 @@ import funny.buildapp.common.database.DriverFactory
 import funny.buildapp.common.database.database
 
 fun main() = application {
+    // Cannot find skiko-windows-x64.dll.sha256, proper native dependency missing
     database =Database(DriverFactory().createDriver())
     Window(onCloseRequest = ::exitApplication, title = "WorkLite") {
         MaterialTheme(colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()) {
